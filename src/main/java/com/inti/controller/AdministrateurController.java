ccff5133790487fed18035e56c64aadf72789bcb
lucieargaud <lucieargaud@gmail.com>
2023-04-12 10:17:12 +0200

    Plus de PersonneAttente + fnctionnalité gestion utilisateurs

--- src/main/java/com/inti/controller/AdministrateurController.java
+++ src/main/java/com/inti/controller/AdministrateurController.java
@@ -1,225 +1,58 @@
 package com.inti.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.inti.model.AdminAttente;
-import com.inti.model.Administrateur;
+import com.inti.model.Personne;
 import com.inti.model.Utilisateur;
-import com.inti.model.UtilisateurAttente;
-import com.inti.model.Proprietaire;
-import com.inti.model.ProprietaireAttente;
-import com.inti.repository.IAdminAttenteRepository;
-import com.inti.repository.IAdministrateurRepository;
-import com.inti.repository.IUtilisateurAttenteRepository;
 import com.inti.repository.IUtilisateurRepository;
-import com.inti.repository.IProprietaireAttenteRepository;
-import com.inti.repository.IProprietaireRepository;
-import com.inti.repository.INoteRepository;
-
 @Controller
 @RequestMapping("admin")
 public class AdministrateurController {
+	
+	@Autowired
+	IUtilisateurRepository iur;
 
-	@Autowired
-	IAdministrateurRepository iar;
+	@GetMapping("listeUtilisateurs")
+	public String listeUtilisateurs(Model m) {
+		m.addAttribute("listeUtilisateurs", iur.findAll());
+		return "listeUtilisateurs";
+	}
+	@GetMapping("getUtilisateur/{id}")
+	public String getVoiture(@PathVariable("id") int id, Model m) {
+		m.addAttribute("utilisateur", iur.findById(id).get());
+		return "getUtilisateur";
+	}
+	@GetMapping("deleteUtilisateur/{id}")
+	public String deleteUtilisateur(@PathVariable("id") int id) {
+		iur.deleteById(id);
+		return "redirect://localhost:8080/admin/listeUtilisateurs";
+	}
 	
-	@Autowired
-	IAdminAttenteRepository iaar;
-	
-	@Autowired
-	IUtilisateurRepository icr;
-	
-	@Autowired
-	IUtilisateurAttenteRepository icar;
-	
-	@Autowired
-	IProprietaireRepository igr; 
-	
-	@Autowired
-	IProprietaireAttenteRepository igar; 
-	
-	@Autowired
-	INoteRepository inr;
-	
-	
-	// Client
-	@PostMapping("ajouterClient")
-	public UtilisateurAttente inscriptionClient(@RequestBody UtilisateurAttente c)
+	@GetMapping("modifierUtilisateur/{id}")
+	public String modifierUtilisateur(@PathVariable("id") int id, Model m)
 	{
-		return icar.save(c);
-	}
-	 
-	@GetMapping("getClientsAttente")
-	public List<UtilisateurAttente> getClientsAttente()
-	{
-		return icar.findAll();
+		System.out.println(id);
+		m.addAttribute("u", iur.findById(id).get());
+		return "modifierUtilisateur";
 	}
 	
-	@DeleteMapping("deleteClientAttente/{id}")
-	public void deleteClientAttente(@PathVariable("id") int id)
+	@PostMapping("updateUtilisateur")
+	public String updateUtilisateur(@ModelAttribute("utilisateur") Utilisateur u)
 	{
-		icar.deleteById(id);
+		BCryptPasswordEncoder b = new BCryptPasswordEncoder();
+		u.setMdp(b.encode(u.getMdp()));
+		System.out.println(u);
+		System.out.println(u.getNom() + u.getLogin());
+		iur.save(u);
+		return "redirect://localhost:8080/admin/listeUtilisateurs";
 	}
-	
-    // Validation du compte client
-	@PostMapping("validerClient")
-	public void validerClientAttente(@RequestBody Utilisateur c)
-	{
-		icr.save(c);
-	}
-	
-	@GetMapping("getClientsValide")
-	public List<Utilisateur> getClients()
-	{
-		return icr.findAll();
-	}
-	
-	@GetMapping("getClientById/{id}")
-	public Utilisateur getClientById(@PathVariable("id") int id)
-	{
-		return icr.getReferenceById(id);
-	}
-	
-	@DeleteMapping("deleteClientValide/{id}")
-	public void deleteClient(@PathVariable("id") int id)
-	{
-		icr.deleteById(id);
-	}
-	
-	@PutMapping("updateClient")
-	public void updateClient(@RequestBody Utilisateur c)
-	{
-		icr.save(c);
-	}
-	
-	// Admin
-	@PostMapping("ajouterAdmin")
-	public AdminAttente inscriptionAdmin(@RequestBody AdminAttente a)
-	{
-		return iaar.save(a);
-	}
-
-	@GetMapping("getAdminsAttente")
-	public List<AdminAttente> getAdminsAttente()
-	{
-		return iaar.findAll();
-	}
-	
-	@DeleteMapping("deleteAdminAttente/{id}")
-	public void deleteAdminAttente(@PathVariable("id") int id)
-	{
-		iaar.deleteById(id);
-	}
-	
-	 // Validation du compte admin	
-	@PostMapping("validerAdmin")
-	public void validerAdminAttente(@RequestBody Administrateur a)
-	{
-		iar.save(a);
-	}
-	
-	@GetMapping("getAdminValide")
-	public List<Administrateur> getAdmins()
-	{
-		return iar.findAll();
-	}
-	
-	@GetMapping("getAdminById/{id}")
-	public Administrateur getAdminById(@PathVariable("id") int id)
-	{
-		return iar.getReferenceById(id);
-	}
-	
-	@DeleteMapping("deleteAdminValide/{id}")
-	public void deleteAdmin(@PathVariable("id") int id)
-	{
-		iar.deleteById(id);
-	}
-	@PutMapping("updateAdmin")
-	public void updateAdmin(@RequestBody Administrateur a)
-	{
-		iar.save(a);
-	}
-
-	
-	// Gerant
-	@PostMapping("ajouterGerant")
-	public ProprietaireAttente inscriptionGerant(@RequestBody ProprietaireAttente g)
-	{
-			
-		return igar.save(g);
-	}
-		
-	@GetMapping("getGerantsAttente")
-	public List<ProprietaireAttente> getGerantsAttente()
-	{
-		return igar.findAll();
-	}
-	
-	@DeleteMapping("deleteGerantAttente/{id}")
-	public void deleteGerantAttente(@PathVariable("id") int id)
-	{
-		igar.deleteById(id);
-	}
-	
-	 // Validation du compte admin
-	@PostMapping("validerGerant")
-	public void validerGerantAttente(@RequestBody Proprietaire g)
-	{
-		igr.save(g);
-	}
-
-	@GetMapping("getGerantsValide")
-	public List<Proprietaire> getGerants()
-	{
-		return igr.findAll();
-	}
-	
-	@GetMapping("getGerantById/{id}")
-	public Proprietaire getGerantById(@PathVariable("id") int id)
-	{
-		return igr.getReferenceById(id);
-	}
-		
-	@DeleteMapping("deleteGerantValide/{id}")
-	public void deleteGerant(@PathVariable("id") int id)
-	{
-		igr.deleteById(id);
-	}
-	
-	@PutMapping("updateGerant")
-	public void updateGerant(@RequestBody Proprietaire g)
-	{
-		igr.save(g);
-	}
-	
-	// Statistiques
-	
-	@GetMapping("moyenneNotes")
-	public double moyenneNotes() {
-	
-		double moy = inr.moyenneNotes();
-		System.out.println("Moyenne notes "+ moy);
-		return moy;
-	}
-	
-	@GetMapping("nombreClients")
-	public long nombreClient()
-	{
-		long n=icr.count();
-		System.out.println("Nombre clients " + n);
-		return n;
-	}
-	
-
 }
